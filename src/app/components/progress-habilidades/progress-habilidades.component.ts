dcd3ccc45780968801ba3046055c8e65e5ece889
francofrighetto <68360116+francofrighetto@users.noreply.github.com>
2023-03-15 17:26:39 -0300

    acomodado

--- src/app/components/progress-habilidades/progress-habilidades.component.ts
+++ src/app/components/progress-habilidades/progress-habilidades.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { DatosPortfolioService } from 'src/app/services/datos-portfolio.service';
 
 @Component({
   selector: 'app-progress-habilidades',
@@ -7,53 +8,40 @@ import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 })
 export class ProgressHabilidadesComponent implements OnInit {
 
-  constructor(private elRef: ElementRef) { }
-  width: number = 0;
-  carga:boolean=false;
+  constructor(private elRef: ElementRef, private datosPortfolio: DatosPortfolioService) { }
+  carga: boolean = false;
+  progreso: any;
 
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    console.log("entro");
     const componentPosition = this.elRef.nativeElement.getBoundingClientRect().top;
     const scrollPosition = window.scrollY;
 
-    if (scrollPosition+300 >= componentPosition) {
-      if (!this.carga){
-      const intervalo = setInterval(() => {
-        this.width++;
-        if (this.width === 20) {
-          clearInterval(intervalo);
+
+    if (scrollPosition + 300 >= componentPosition) {
+      if (!this.carga) {
+
+      for (let i of this.progreso) {
+          const intervalo = setInterval(() => {
+            i.porcentaje_inicial = parseInt(i.porcentaje_inicial)+1;
+            if (parseInt(i.porcentaje_inicial) >= parseInt(i.porcentaje)) {
+              clearInterval(intervalo);
+            }
+          }, 10);
+          
         }
-      }, 20);
+        this.carga = true;
+      }
     }
-    this.carga=true;
-  }
   }
 
   ngOnInit(): void {
-    // var bottom_of_window = $(window).scrollTop() + $(window).height();
-    // var middle_of_developmentWrapper = $developmentWrapper.offset().top + $developmentWrapper.outerHeight()
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      this.progreso = data.barra_progreso;
 
-    // const developmentWrapper = document.querySelector(".development-wrapper") as HTMLElement;
-    // const bottom_of_window = window.scrollY;
-    // const middle_of_developmentWrapper = developmentWrapper.offsetHeight;
-    // console.log(bottom_of_window, middle_of_developmentWrapper);
-
-
-
-    // if (true) {
-    //   const intervalo = setInterval(() => {
-    //     this.width++;
-    //     // console.log('El contador es:', this.width);
-    //     if (this.width === 20) {
-    //       clearInterval(intervalo);
-    //     }
-    //   }, 20);
-    // }
+    });
   }
 }
-function onWindowScroll() {
-  throw new Error('Function not implemented.');
-}
+
 
