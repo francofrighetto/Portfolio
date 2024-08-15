ba74f67e7016c82300484605780a1c6ee2c65a77
francofrighetto <frighetto23@gmail.com>
2024-08-15 17:02:56 -0300

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   firebaseConfig : {
     apiKey: "AIzaSyBVcgfSZfVOk4U4Xsdv5DUZfs8gsK7MehI",
-    authDomain: "angular-portfolio14.firebaseapp.com",
-    projectId: "angular-portfolio14",
-    storageBucket: "angular-portfolio14.appspot.com",
+    authDomain: "francofrighetto.firebaseapp.com",
+    projectId: "francofrighetto",
+    storageBucket: "francofrighetto.appspot.com",
     messagingSenderId: "350590704284",
     appId: "1:350590704284:web:1706e36f9fe061c9966b59",
     measurementId: "G-R736BMT1XG"
